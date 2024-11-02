64a1e68c25a09b1f76fef221314f27ac7bed7c0a
FernandoKeinOjedaLeonardo <fojedaleonardo@gmail.com>
2024-11-01 20:55:49 -0500

    Se agrega componentes para las páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { CartaComponent } from './pages/carta/carta.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { LocalesComponent } from './pages/locales/locales.component';
+import { AccederComponent } from './pages/acceder/acceder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "inicio", component: InicioComponent},
+    {path: "carta", component: CartaComponent},
+    {path: "equipo", component: EquipoComponent},
+    {path: "contacto", component: ContactoComponent},
+    {path: "locales", component: LocalesComponent},
+    {path: "acceder", component: AccederComponent}
+
+];
---
+++ src/app/pages/acceder/acceder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acceder',
+  standalone: true,
+  imports: [],
+  templateUrl: './acceder.component.html',
+  styleUrl: './acceder.component.css'
+})
+export class AccederComponent {
+
+}
